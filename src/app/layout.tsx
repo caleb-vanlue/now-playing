ffcb34d28381eae7e03f7b4a90ec469558b17732
Caleb Van Lue <vanluecaleb@outlook.com>
2025-05-11 19:34:57 -0400

    enable web analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { MediaDataProvider } from "../components/MediaDataContext";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body
         className={`${inter.variable} antialiased bg-[#141414] text-white font-sans`}
       >
+        <Analytics />
         <MediaDataProvider>{children}</MediaDataProvider>{" "}
       </body>
     </html>
